5d97284e2a3604b249f5a71ab6a619181e42616b
Hari <rharikrishnan2020@gmail.com>
2025-12-26 13:46:10 +0530

    Initial Commit

--- imageprocess.py
+++ imageprocess.py
@@ -68,11 +68,11 @@ if uploaded_file:
     col1, col2 = st.columns(2)
 
     with col1:
-        if st.button("Previous"):
+        if st.button(" <-Previous"):
             st.session_state.step = (st.session_state.step - 1) % len(results)
 
     with col2:
-        if st.button("Next"):
+        if st.button("Next-> "):
             st.session_state.step = (st.session_state.step + 1) % len(results)
 
 else:
